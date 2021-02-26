7e94904231b4dbce886276f5ec6391f30a1ef2f7
rinatisk <st087501@student.spbu.ru>
2021-02-26 21:00:39 +0300

    Add 1 task from 1 homework

---
+++ src/main/kotlin/homework_1/task_1.kt
@@ -0,0 +1,41 @@
+package homework_1
+
+import java.util.*
+
+fun getFactorialRecursive(number: Int): Int {
+    return when {
+        number == 1 -> 1
+        number > 1 -> number * getFactorialRecursive(number - 1)
+        else -> -1
+    }
+}
+
+fun getFactorialIterative(number: Int): Int {
+    if (number < 1) return -1
+    return (1..number).reduce { first, second -> first * second }
+}
+
+fun getInputNumber(): Int {
+    println("Please write down your number to get factorial")
+    val scan = Scanner(System.`in`)
+
+    if (!scan.hasNextInt()) {
+        println("Invalid input: you write non-number")
+        return -1
+    }
+    val numberToGetFactorial: Int = scan.nextInt()
+    if (numberToGetFactorial < 1) {
+        println("Invalid input: you write non-natural number")
+        return -1
+    }
+
+    return numberToGetFactorial
+}
+
+fun main() {
+
+    val numberToGetFactorial: Int = getInputNumber()
+
+    if (numberToGetFactorial > 0) print("recursive factorial $numberToGetFactorial! = ${getFactorialRecursive(numberToGetFactorial)}\n" +
+            "iterative factorial $numberToGetFactorial! = ${getFactorialIterative(numberToGetFactorial)}")
+}
