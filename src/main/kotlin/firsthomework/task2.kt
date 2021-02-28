e6215dac1f81cdcd9f7723c684c9274e198bd760
rinatisk <st087501@student.spbu.ru>
2021-02-28 10:44:22 +0300

    fix exceptions

--- src/main/kotlin/firsthomework/task2.kt
+++ src/main/kotlin/firsthomework/task2.kt
@@ -7,22 +7,16 @@ fun main() {
     val scan = Scanner(System.`in`)
     println("Please write string and substring throw a line:")
 
-    val mainString = try {
-        scan.nextLine()
-    } catch (e: IllegalArgumentException) {
-        error("you didn't write main string")
-    }
+    val mainString = scan.nextLine()
 
-    val substring = try {
-        scan.nextLine()
-    } catch (e: IllegalArgumentException) {
-        error("you didn't write substring")
-    }
+    val substring = scan.nextLine()
 
     val answer: Int = try {
         mainString.windowed(substring.length) {
         if (it.equals(substring)) 1 else 0 }.sum()
-    } catch (e: IllegalArgumentException) { error("you didn't write main string") }
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException("you didn't write string")
+    }
 
     print("substring include in string $answer times")
 }
