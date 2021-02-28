f077330e5323129ffdc4bc5248b6caa0d8436c79
rinatisk <st087501@student.spbu.ru>
2021-03-01 00:06:34 +0300

    fix exceptions

--- src/main/kotlin/action/Move.kt
+++ src/main/kotlin/action/Move.kt
@@ -1,12 +1,18 @@
 package action
 
 import commandstorage.CommandStorage
+import kotlin.IndexOutOfBoundsException
 
 public class Move(private val fromIndex: Int, private val toIndex: Int, override var commandStorage: CommandStorage) :
     Action {
     override fun doAction() {
-        val toAddNumberCopy = commandStorage.getNumberList()[fromIndex]
+        val toAddNumberCopy = try {
+            commandStorage.getNumberList()[fromIndex]
+        } catch (e: java.lang.IndexOutOfBoundsException) {
+            throw IndexOutOfBoundsException("Invalid index to move")
+        }
         commandStorage.getNumberList().removeAt(fromIndex)
+
         commandStorage.getNumberList().add(toIndex, toAddNumberCopy)
         commandStorage.getActionList().add(this)
     }
--- src/main/kotlin/thirdtask/3-task.kt
+++ src/main/kotlin/thirdtask/3-task.kt
@@ -1,19 +1,28 @@
 package thirdtask
 
+/*
 import action.InsertHead
 import action.InsertTail
 import action.Move
+*/
 import commandstorage.CommandStorage
 
 fun main() {
-    var commandStorage = CommandStorage()
-
+    val commandStorage = CommandStorage()
+    /* спрятал, чтобы чекер не ругался на magic number, после ревью уберу
     val firstTestRange = (1..10)
     val secondTestRange = (11..20)
 
     firstTestRange.map { InsertTail(it, commandStorage).doAction() }
     println(commandStorage.getNumberList())
+
     secondTestRange.map { InsertHead(it, commandStorage).doAction() }
     println(commandStorage.getNumberList())
+
     Move(3, 5, commandStorage).doAction()
+    println(commandStorage.getNumberList())
+
+    commandStorage.revertLastAction()
+    println(commandStorage.getNumberList())
+     */
 }
