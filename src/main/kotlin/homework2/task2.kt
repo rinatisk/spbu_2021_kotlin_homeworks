20233bcf62ac6b7c4d2f3a313f9685dfd4a9f03e
rinatisk <st087501@student.spbu.ru>
2021-03-09 00:23:09 +0300

    add solution for 2 task from 2 hw

--- src/main/kotlin/homework2/task2.kt
+++ src/main/kotlin/homework2/task2.kt
@@ -4,20 +4,14 @@ import java.io.File
 import java.util.Scanner
 
 fun main() {
-    val list = getUserArray()
-  //  list.removeRepeatedElements()
-    println(list)
+    val list = getUserList()
+    if (list.isEmpty()) println("write numbers to input.txt")
+    println(list.reversed().distinct().reversed())
 }
 
-fun MutableList<Int>.removeRepeatedElements() {
-    this.map { if (this.contains(it)) this.remove(element = it) }
-}
-
-fun getUserArray(): MutableList<Int> {
-    val userList = mutableListOf<Int>()
+fun getUserList(): MutableList<Int> {
+    val userList = arrayListOf<Int>()
     val scan = Scanner(File("src/main/kotlin/homework2/input.txt"))
-  //  val input = File("input.txt").readBytes()
- //   userList[0] = scan.nextInt()
     while (scan.hasNextInt()) {
         userList.add(scan.nextInt())
     }
