5eed9f1c623a92960e2d41d07f8db296e5d6e867
rinatisk <70975579+rinatisk@users.noreply.github.com>
2021-03-09 19:57:07 +0300

    change path to get resources

--- src/main/kotlin/homework2/task2.kt
+++ src/main/kotlin/homework2/task2.kt
@@ -6,12 +6,15 @@ import java.util.Scanner
 fun main() {
     val list = getUserList()
     if (list.isEmpty()) println("write numbers to input.txt")
-    println(list.reversed().distinct().reversed())
+    println(list.removeDuplicates())
 }
+
+fun ArrayList<Int>. removeDuplicates() = this.reversed().distinct().reversed()
 
 fun getUserList(): ArrayList<Int> {
     val userList = arrayListOf<Int>()
-    val scan = Scanner(File("src/main/kotlin/homework2/input.txt"))
+    val resource = object {}.javaClass.getResource("input.txt").file
+    val scan = Scanner(File(resource))
     while (scan.hasNextInt()) {
         userList.add(scan.nextInt())
     }
