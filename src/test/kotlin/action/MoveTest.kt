57f87e4fccdd07bd8bdbfbb04d1c62fe15eb219b
rinatisk <st087501@student.spbu.ru>
2021-03-16 17:14:53 +0300

    add first param test

--- src/test/kotlin/action/MoveTest.kt
+++ src/test/kotlin/action/MoveTest.kt
@@ -4,8 +4,27 @@ import commandstorage.CommandStorage
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class MoveTest {
+
+    companion object {
+        @JvmStatic
+        fun inputData(): List<Arguments> = listOf(
+            Arguments.of(listOf(1, 4, 2, 3), Move(3, 1)),
+            Arguments.of(listOf(1, 2, 3, 4), Move(0, 0)))
+    }
+
+    @MethodSource("inputData")
+    @ParameterizedTest(name = "test {index}, {1}")
+    fun paramDoAction(expected: List<Int>, inputAction: Action) {
+        val commandStorage = CommandStorage()
+        (1..4).map { commandStorage.doAction(InsertTail(it)) }
+        commandStorage.doAction(inputAction)
+        assertEquals(expected, commandStorage.numberList)
+    }
 
     @Test
     fun doAction() {
