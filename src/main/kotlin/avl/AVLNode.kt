046777fcdf751e0a4e0d08168e89bfadf26c65b8
rinatisk <st087501@student.spbu.ru>
2021-04-07 22:08:31 +0300

    init

---
+++ src/main/kotlin/avl/AVLNode.kt
@@ -0,0 +1,132 @@
+package avl
+
+class AVLNode<K : Comparable<K>, V>(private val keyNode: K, private var valueNode: V) : Map.Entry<K, V> {
+    private var left: AVLNode<K, V>? = null
+    private var right: AVLNode<K, V>? = null
+
+    override val key: K
+        get() = keyNode
+    override val value: V
+        get() = valueNode
+
+    private val height: Int
+        get() = kotlin.math.max((left?.height ?: 0), (right?.height ?: 0)) + 1
+
+    private val balanceFactor: Int
+        get() = (right?.height ?: 0) - (left?.height ?: 0)
+
+    private fun rotateLeft(): AVLNode<K, V>? {
+        val toChangeNode = this.right
+        this.right = toChangeNode?.left
+        toChangeNode?.left = this
+        return toChangeNode
+    }
+
+    fun insertNode(key: K, value: V): AVLNode<K, V>? = when {
+        key == this.keyNode -> {
+            val current = this
+            this.valueNode = value
+            current
+        }
+        key < this.keyNode -> when (this.left) {
+            null -> {
+                this.left = AVLNode(key, value)
+                null
+            }
+            else -> {
+                left?.insertNode(key, value)
+            }
+        }
+        key > this.keyNode -> when (this.right) {
+            null -> {
+                this.right = AVLNode(key, value)
+                null
+            }
+            else -> {
+                right?.insertNode(key, value)
+            }
+        }
+        else -> null
+    }
+
+    fun balance(): AVLNode<K, V>? {
+        left = left?.balance()
+        right = right?.balance()
+        return when {
+            this.balanceFactor > 1 -> {
+                if (right?.balanceFactor ?: 1 < 0) {
+                    right = right?.rotateRight()
+                }
+                this.rotateLeft()
+            }
+            this.balanceFactor < -1 -> {
+                if (left?.balanceFactor ?: -1 > 0) {
+                    left = left?.rotateLeft()
+                }
+                this.rotateRight()
+            }
+            else -> this
+        }
+    }
+
+    fun removeNode(key: K): AVLNode<K, V>? = when {
+        key < this.keyNode -> {
+            left = left?.removeNode(key)
+            this
+        }
+        key > this.keyNode -> {
+            right = right?.removeNode(key)
+            this
+        }
+        else -> when {
+            left == null -> right
+            right == null -> left
+            else -> {
+                val minimalNode: AVLNode<K, V> = right?.getMinimalRecursive() ?: this
+                minimalNode.left = this.left
+                if (minimalNode != this.right) {
+                    this.right?.removeMinimal(minimalNode)
+                    minimalNode.right = this.right
+                }
+                minimalNode.balance()
+            }
+        }
+    }
+
+    private fun rotateRight(): AVLNode<K, V>? {
+        val toChangeNode = this.left
+        this.left = toChangeNode?.right
+        toChangeNode?.right = this
+        return toChangeNode
+    }
+
+    fun nodeFromKey(key: K): AVLNode<K, V>? = when {
+        key == this.keyNode -> this
+        key > this.keyNode -> this.right?.nodeFromKey(key)
+        key < this.keyNode -> this.left?.nodeFromKey(key)
+        else -> null
+    }
+
+    private fun getMinimalRecursive(): AVLNode<K, V> = left?.getMinimalRecursive() ?: this
+
+    private fun removeMinimal(minimalNode: AVLNode<K, V>?) {
+        when (left) {
+            minimalNode -> {
+                left = minimalNode?.right
+            }
+            else -> {
+                left?.removeMinimal(minimalNode)
+            }
+        }
+    }
+
+    fun containsValueNode(value: V): Boolean =
+        this.valueNode == value ||
+                left?.containsValueNode(value) ?: false || right?.containsValueNode(value) ?: false
+
+    fun entriesNode(entries: MutableSet<AVLNode<K, V>>) {
+        entries += this
+        left?.entriesNode(entries)
+        right?.entriesNode(entries)
+    }
+}
---
+++ src/main/kotlin/avl/AVLTree.kt
@@ -0,0 +1,57 @@
+package avl
+
+class AVLTree<K : Comparable<K>, V>
+    (var root: AVLNode<K, V>? = null) : Map<K, V> {
+
+    override var size: Int = 0
+
+    override val entries: Set<Map.Entry<K, V>>
+        get() {
+            val entries = mutableSetOf<AVLNode<K, V>>()
+            root?.entriesNode(entries) ?: emptySet<AVLNode<K, V>>()
+            return entries
+        }
+
+    override val keys: Set<K>
+        get() = this.entries.map { it.key }.toSet()
+
+    override val values: Collection<V>
+        get() = this.entries.map { it.value }.toList()
+
+    override fun get(key: K) = root?.nodeFromKey(key)?.value
+
+    override fun containsValue(value: V) = root?.containsValueNode(value) ?: false
+    override fun containsKey(key: K) = get(key) != null
+
+    override fun isEmpty() = (size == 0)
+
+    fun insert(key: K, value: V): AVLNode<K, V>? =
+        when {
+            this.isEmpty() -> {
+                root = AVLNode(key, value)
+                size++
+                null
+            }
+            else -> {
+                when (val insertResult = root?.insertNode(key, value)) {
+                    null -> {
+                        size++
+                        root = root?.balance()
+                        null
+                    }
+                    else -> insertResult
+                }
+            }
+        }
+
+    fun remove(key: K): V? {
+        val toRemove: V? = get(key)
+
+        if (toRemove != null) size--
+
+        root = root?.removeNode(key)
+        this.root?.balance()
+
+        return toRemove
+    }
+}
