402d69ec638e0634154dd901c7423d851d05a83e
rinatisk <st087501@student.spbu.ru>
2021-04-16 12:59:59 +0300

    try to fix errors

--- src/main/kotlin/tornadoFx/tic-tac-toe.kt
+++ src/main/kotlin/tornadoFx/tic-tac-toe.kt
@@ -1,23 +1,26 @@
 package tornadoFx
-import javafx.application.Application
-import tornadofx.*
+import tornadofx.Fragment
+import tornadofx.label
+import tornadofx.View
+import tornadofx.tableview
+import tornadofx.App
+import tornadofx.launch
 
 enum class Mark {
     O, X
 }
 
-class Cell(type: Mark): Fragment() {
+class Cell(type: Mark) : Fragment() {
     override val root = label(type.name)
 }
 
-class Board: View("Board") {
+class Board : View("Board") {
     override val root = tableview<Cell> {
     }
 }
 
-class Game: App(Board::class) {
-}
+class Game : App(Board::class)
 
 fun main(args: Array<String>) {
-    launch<Game>(args) }
-
+    launch<Game>(args)
+}
