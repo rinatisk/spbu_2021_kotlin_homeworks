8e49d09faba30973dfd565ab399f7d44c5a5e749
rinatisk <70975579+rinatisk@users.noreply.github.com>
2021-05-30 23:17:45 +0300

    update tests

---
+++ src/test/kotlin/homework6/MergeSorterTest.kt
@@ -0,0 +1,35 @@
+package homework6
+
+import homework6.MergeSorter.mergeSort
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class MergeSorterTest {
+    companion object {
+
+        private fun getRandomList(size: Int): MutableList<Int> = MutableList(size) { kotlin.random.Random.nextInt() }
+
+        @JvmStatic
+        fun testSort(): List<Arguments> {
+            val argumentsList = mutableListOf<Arguments>()
+            for (threadNumber in 1..8) {
+                for (size in 0..10000 step 2000) {
+                    val list = getRandomList(size)
+                    argumentsList.add(Arguments.of(threadNumber, list, list.sorted()))
+                }
+            }
+            return argumentsList
+        }
+    }
+
+    @MethodSource("testSort")
+    @ParameterizedTest(name = "sort{index}, {1}")
+    fun sort(numberOfThreads: Int, toSortList: MutableList<Int>, expectedList: MutableList<Int>) {
+        val actual = toSortList.mergeSort(numberOfThreads)
+
+        assertEquals(expectedList, actual)
+    }
+
+}
