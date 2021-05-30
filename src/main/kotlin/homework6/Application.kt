117a8dac7b3ffb24aee76885ca0e486e4fe26654
rinatisk <70975579+rinatisk@users.noreply.github.com>
2021-05-30 17:48:06 +0300

    decompose init fun

--- src/main/kotlin/homework6/Application.kt
+++ src/main/kotlin/homework6/Application.kt
@@ -21,26 +21,67 @@ import kotlin.math.log2
 import kotlin.math.pow
 
 fun main() {
-    Application(8)
+    Application(8, 1000, 50000)
 }
 
-class Application(private val numberOfThreads: Int = 4) {
+class Application(private val numberOfThreads: Int = 4, private val stepNumber: Int, private val maxSize: Int) {
+
+    private class UpdateAction(val app: Application, plot: CombinedDomainXYPlot, i: Int, val stepNumber: Int, val maxSize: Int) : AbstractAction("Update plot ") {
+        private val plot: XYPlot = plot.subplots[i] as XYPlot
+        override fun actionPerformed(e: ActionEvent) {
+            plot.dataset = app.generateData(stepNumber, maxSize)
+        }
+    }
+
+    private class VisibleAction(private val renderer: XYItemRenderer, private val i: Int) :
+        AbstractAction("Threads ${2.0.pow(i).toInt()}") {
+        override fun actionPerformed(e: ActionEvent) {
+            renderer.setSeriesVisible(i, !renderer.getSeriesVisible(i))
+        }
+    }
 
      init {
         val renderer: XYItemRenderer = StandardXYItemRenderer(StandardXYItemRenderer.SHAPES_AND_LINES)
+        val plot = generatePlot(renderer)
+        val chartPanel = generateChartPanel(plot)
+        val controlPanel = generateControlPanel(plot)
+        generateJCheckBox(renderer, controlPanel)
+        generateFrame("Mt Merge Sort", chartPanel, controlPanel)
+    }
+
+    private fun generatePlot(renderer: XYItemRenderer): CombinedDomainXYPlot {
         val plot = CombinedDomainXYPlot(NumberAxis("Size"))
         plot.isRangePannable = true
         plot.add(XYPlot(
-            generateData(), null, NumberAxis("Time"), renderer
+            generateData(stepNumber, maxSize), null, NumberAxis("Time"), renderer
         ))
         plot.orientation = PlotOrientation.VERTICAL
-        val chart = JFreeChart(
-            "Mt Merge Sort", JFreeChart.DEFAULT_TITLE_FONT, plot, true
-        )
-        val chartPanel = ChartPanel(chart)
-        chartPanel.preferredSize = Dimension(1000, 600)
-        val controlPanel = JPanel()
-        controlPanel.add(JButton(UpdateAction(this, plot, 0)))
+        return plot
+    }
+
+    private fun generateData(stepNumber: Int, maxSize: Int): XYSeriesCollection {
+        val data = XYSeriesCollection()
+        var i = 1
+        while (i <= numberOfThreads) {
+            data.addSeries(generateSeries("Threads $i", i, stepNumber, maxSize))
+            i *= 2
+        }
+        return data
+    }
+
+    private fun getRandomList(size: Int): MutableList<Int> = MutableList(size) { kotlin.random.Random.nextInt() }
+
+    private fun generateFrame(title: String, chartPanel: ChartPanel, controlPanel: JPanel) {
+        val frame = JFrame(title)
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.add(chartPanel, BorderLayout.CENTER)
+        frame.add(controlPanel, BorderLayout.SOUTH)
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.isVisible = true
+    }
+
+    private fun generateJCheckBox(renderer: XYItemRenderer, controlPanel: JPanel) {
         var i = 1
         while (i <= numberOfThreads) {
             val jcb = JCheckBox(VisibleAction(renderer, log2(i.toDouble()).toInt()))
@@ -50,32 +91,28 @@ class Application(private val numberOfThreads: Int = 4) {
             controlPanel.add(jcb)
             i *= 2
         }
-        val frame = JFrame("Mt Merge Sort")
-        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        frame.add(chartPanel, BorderLayout.CENTER)
-        frame.add(controlPanel, BorderLayout.SOUTH)
-        frame.pack()
-        frame.setLocationRelativeTo(null)
-        frame.isVisible = true
     }
 
-    fun generateData(): XYSeriesCollection {
-        val data = XYSeriesCollection()
-        var i = 1
-        while (i <= numberOfThreads) {
-            data.addSeries(generateSeries("Threads $i", i, 1000, 50000))
-            i *= 2
-        }
-        return data
+    private fun generateControlPanel(plot: CombinedDomainXYPlot): JPanel {
+        val controlPanel = JPanel()
+        controlPanel.add(JButton(UpdateAction(this, plot, 0, stepNumber, maxSize)))
+        return controlPanel
     }
 
-    private fun randomList(size: Int): MutableList<Int> = MutableList(size) { kotlin.random.Random.nextInt() }
+    private fun generateChartPanel(plot: CombinedDomainXYPlot): ChartPanel {
+        val chart = JFreeChart(
+            "Mt Merge Sort", JFreeChart.DEFAULT_TITLE_FONT, plot, true
+        )
+        val chartPanel = ChartPanel(chart)
+        chartPanel.preferredSize = Dimension(1000, 600)
+        return chartPanel
+    }
 
     private fun generateSeries(key: String, numberOfThreads: Int, stepNumber: Int, maxSize: Int): XYSeries {
         val series = XYSeries(key)
             for (size in 1000..maxSize step stepNumber) {
                 val startTime = System.nanoTime()
-                randomList(size).sort(numberOfThreads)
+                getRandomList(size).sort(numberOfThreads)
                 val endTime = System.nanoTime()
                 series.add(size, (endTime - startTime) / 100_000)
             }
@@ -84,16 +121,3 @@ class Application(private val numberOfThreads: Int = 4) {
     }
 }
 
-private class UpdateAction(val app: Application, plot: CombinedDomainXYPlot, i: Int) : AbstractAction("Update plot ") {
-    private val plot: XYPlot = plot.subplots[i] as XYPlot
-    override fun actionPerformed(e: ActionEvent) {
-        plot.dataset = app.generateData()
-    }
-}
-
-private class VisibleAction(private val renderer: XYItemRenderer, private val i: Int) :
-    AbstractAction("Threads ${2.0.pow(i).toInt()}") {
-    override fun actionPerformed(e: ActionEvent) {
-        renderer.setSeriesVisible(i, !renderer.getSeriesVisible(i))
-    }
-}
